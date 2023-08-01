6eb4cbd0ed23b170eee170ab856b192ab0f46124
luttoli <hhn0212@naver.com>
2023-08-01 20:48:11 +0900

    first commit

---
+++ mytodolist/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mytodolist
+//
+//  Created by 김지훈 on 2023/08/01.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
