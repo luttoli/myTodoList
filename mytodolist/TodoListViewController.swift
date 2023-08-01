0ff231f19fd979c605f8b421c7a50103b1661edf
luttoli <hhn0212@naver.com>
2023-08-01 23:08:37 +0900

    네비게이션컨트롤러 추가, 할일, 완료한일 페이지 생성 및 버튼 클릭 시 이동 구현

---
+++ mytodolist/TodoListViewController.swift
@@ -0,0 +1,29 @@
+//
+//  TodoListViewController.swift
+//  mytodolist
+//
+//  Created by 김지훈 on 2023/08/01.
+//
+
+import UIKit
+
+class TodoListViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
